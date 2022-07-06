b750bbd594912446b7181f0061ba81afcc8c9603
AlexCastillaG <alcasga2@epsg.upv.es>
2022-07-06 12:56:58 +0200

    mean added to the RTRplotter

--- RealTimeRadarPlotter.m
+++ RealTimeRadarPlotter.m
@@ -4,18 +4,19 @@ clc
 
 data_length = 1083;
 fft_length = 503;
-u = udpport("IPV4");
+u = udpport("IPV4",Timeout=1);
 hello= "CCC";
 x_axis=linspace(1,fft_length,fft_length);
 figure(1)
-
 
 while true
     t0 = clock;
     write(u,hello,"string","192.168.2.237",16000);
     
         while etime(clock, t0) < 7
-            data = dec2hex(read(u,1,"uint8"));
+            data_mean=[];
+            while size(data_mean,1) < 2
+                data = dec2hex(read(u,1,"uint8"));
                 if data == char("2")
                     data = dec2hex(read(u,1,"uint8"));
                     if data == char("20")
@@ -24,11 +25,17 @@ while true
                         if data == char("FD")
                             data = read(u,data_length,"uint8");
                             data=data(10:512);
-                            plot(x_axis,data)
-                            axis([ 10, fft_length, 0 , 128 ]);
-                            drawnow;
+                            data_mean=[data_mean;data];
+
                         end
                 end
+
+            end
+            
+            data_mean=mean(data_mean)
+            plot(x_axis,data_mean)
+            axis([ 10, fft_length, 0 , 128 ]);
+            drawnow;
         end
 end
 
